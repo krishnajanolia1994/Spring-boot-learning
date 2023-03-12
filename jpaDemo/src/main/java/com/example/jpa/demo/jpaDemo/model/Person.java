387f12e2910e473bc27c13b528f7edfa1e791d7b
Krishna Janoliya <krishna.janola@gmail.com>
2023-03-12 23:57:01 +0530

    code committed

---
+++ jpaDemo/src/main/java/com/example/jpa/demo/jpaDemo/model/Person.java
@@ -0,0 +1,32 @@
+package com.example.jpa.demo.jpaDemo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+	@Id
+	private long id;
+	
+	@Column(name = "person_name")
+	private String name;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
