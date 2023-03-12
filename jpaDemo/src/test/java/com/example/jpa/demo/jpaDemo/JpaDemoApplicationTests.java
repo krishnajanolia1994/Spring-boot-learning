693ab145b202ff056fc492ef5eaddc93fc513c72
Krishna Janoliya <krishna.janola@gmail.com>
2023-03-12 15:53:17 +0530

    code committed

---
+++ jpaDemo/src/test/java/com/example/jpa/demo/jpaDemo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.demo.jpaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
